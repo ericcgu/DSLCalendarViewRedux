70e1c6a6220efa60d4b5529fd3bfee6f4448229d
Eric Gu <eric.changning.gu@gmail.com>
2015-05-08 09:27:23 -0400

    Initial Commit

---
+++ DSLCalendarViewRedux/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DSLCalendarViewRedux
+//
+//  Created by Eric Gu on 5/8/15.
+//  Copyright (c) 2015 egu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
